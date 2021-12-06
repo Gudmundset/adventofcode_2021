02f4b5765da9c51278be836fde2514c9aaa85a79
Philip A Larson <dynendal@gmail.com>
2021-12-05 21:20:38 -0800

    Update and rename day2_part1.py to day2_part1part2.py

--- day2_part1part2.py
+++ day2_part1part2.py
@@ -6,7 +6,7 @@ def translate_input(command):
   direction, amount = command.split(' ')
   return direction, int(amount)
 
-def main():
+def part1():
   
   z_pos = 0
   y_pos = 0
@@ -26,5 +26,29 @@ def main():
   
   print(z_pos * y_pos)
 
+def part2():
+  
+  z_pos = 0
+  y_pos = 0
+  aim = 0
+  command_map = {"forward": operator.add,
+                 "down": operator.add,
+                 "up": operator.sub,
+  }
+  with open("final_input") as f:
+    attempt = f.read()
+  for a in attempt.split('\n'):
+    command, value = translate_input(a)
+    op = command_map[command]
+    if command == 'forward':
+      z_pos = op(z_pos, value)
+      y_pos = y_pos + (aim * value)
+    else:
+      # y_pos = op(y_pos, value)
+      aim = op(aim, value)
+  
+  print(z_pos*y_pos)
+
 if __name__ == "__main__":
-  main()
+  part1()
+  part2()
