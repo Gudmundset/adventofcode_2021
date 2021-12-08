b8dfa277c8492c68c97a66f91b6343a2661bf4d5
Philip A Larson <dynendal@gmail.com>
2021-12-08 00:05:07 -0800

    Update and rename day4_part1.py to day4_part1part2.py

--- day4_part1part2.py
+++ day4_part1part2.py
@@ -3,9 +3,6 @@
 import pprint
 from collections import Counter
 
-def count_me(inp, score):
-  return int(inp)
-
 def scoring(winning_board):
   score = 0
   for row in winning_board:
@@ -24,8 +21,8 @@ def calc_score(bingo_board):
           y_count[f'y{ypos}'] += 1
       returnable = [k for k,v in y_count.items() if v >= 5]
       if returnable: 
-        print(y_count)
-        return board, bingo
+        #print(y_count)
+        return board, bingo, y_count
         
 def mark_x(number):
   return "x"
@@ -38,7 +35,6 @@ def mark_board(number, bingo_board):
           row[pos] = mark_x(r)
 
 def main():
-
   with open("final_input") as f:
     attempt = f.read()
 
@@ -61,10 +57,15 @@ def main():
     mark_board(number, bingo_board)
     output = calc_score(bingo_board)
     if output: 
-      print(output)
+      print(output[0], output[2])
       print(int(number) * scoring(output[1]))
-      break
-  
+      # part 2
+      # 10 Counter({'y4': 5, 'x0': 4, 'y2': 4, 'y3': 4, 'x1': 4, 'y0': 4, 'x2': 4, 'x3': 4, 'x4': 4, 'y1': 3})
+      #23670 - the last number with decent output
+      bingo_board.pop(output[0])
+      # part 1
+      #break
+      
   #pprint.pprint(bingo_board)
   #calc_score(bingo_board)
 
